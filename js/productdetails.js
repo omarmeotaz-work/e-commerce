8b349af18d25a377c61798be91fe01e7059818a4
omarmeotaz-work <omarmoetaz1997@gmail.com>
2025-03-03 09:51:34 +0200

    responsive updated

--- js/productdetails.js
+++ js/productdetails.js
@@ -3,6 +3,11 @@ const signoutButton = document.getElementById("signoutButton");
 const accountIcon = document.getElementById("account");
 const accountdropdown = document.getElementById("dropdownMenuButton");
 const accessToken = JSON.parse(localStorage.getItem("token"));
+const homelogo = document.getElementById("homelogo");
+
+homelogo.addEventListener("click", function () {
+  window.location.href = "http://127.0.0.1:5500/";
+});
 
 loginButton.addEventListener(`click`, function () {
   window.location.href = "http://127.0.0.1:5500/pages/login.html";
