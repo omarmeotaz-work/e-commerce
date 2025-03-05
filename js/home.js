1a65e15f32e06ec03824665abea9dedb51b59682
omarmeotaz-work <omarmoetaz1997@gmail.com>
2025-03-05 13:43:17 +0200

    homepage fixes

--- js/home.js
+++ js/home.js
@@ -35,32 +35,12 @@ function isLoggedIN() {
 
 isLoggedIN();
 
-const revholder = document.getElementById("revholder");
-
-$(revholder).slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-});
-
 async function displayProducts() {
-  let product1 = document.getElementById("p1");
-  let product2 = document.getElementById("p2");
-  let product3 = document.getElementById("p3");
-  let product4 = document.getElementById("p4");
-  let product5 = document.getElementById("p5");
-  let product6 = document.getElementById("p6");
-  let product7 = document.getElementById("p7");
-  let product8 = document.getElementById("p8");
+  let products = document.querySelectorAll("#pTitle");
 
-  let product1p = document.getElementById("p1p");
-  let product2p = document.getElementById("p2p");
-  let product3p = document.getElementById("p3p");
-  let product4p = document.getElementById("p4p");
-  let product5p = document.getElementById("p5p");
-  let product6p = document.getElementById("p6p");
-  let product7p = document.getElementById("p7p");
-  let product8p = document.getElementById("p8p");
+  let productprices = document.querySelectorAll("#pPrices");
+  console.log(products);
+  console.log(productprices);
 
   const response = await fetch(
     "https://dummyjson.com/products?limit=8&skip=8&select=title,price"
@@ -70,24 +50,11 @@ async function displayProducts() {
     return;
   }
   const data = await response.json();
-
-  product1.textContent = data.products[0].title;
-  product2.textContent = data.products[1].title;
-  product3.textContent = data.products[2].title;
-  product4.textContent = data.products[3].title;
-  product5.textContent = data.products[4].title;
-  product6.textContent = data.products[5].title;
-  product7.textContent = data.products[6].title;
-  product8.textContent = data.products[7].title;
-
-  product1p.textContent = data.products[0].price;
-  product2p.textContent = data.products[1].price;
-  product3p.textContent = data.products[2].price;
-  product4p.textContent = data.products[3].price;
-  product5p.textContent = data.products[4].price;
-  product6p.textContent = data.products[5].price;
-  product7p.textContent = data.products[6].price;
-  product8p.textContent = data.products[7].price;
+  const productdata = data.products;
+  for (let i = 0; i <= productdata.length; i++) {
+    products[i].textContent = productdata[i]?.title;
+    productprices[i].textContent = productdata[i]?.price;
+  }
 }
 displayProducts();
 
@@ -107,14 +74,15 @@ async function displayReviews() {
     if (data && data.comments) {
       for (let i = 0; i < data.comments.length; i++) {
         const comment = data.comments[i];
+        const likes = data.comments[i].likes;
         if (!comment) continue;
 
         const reviewDev = `<div class="revbox">
-            <div id="rating" class="rating mb-1">
-                <i class="fa-regular fa-star"></i>
+            <div id="likes" class="likes mb-1">
+                <p>${likes} <i class="fa-solid fa-thumbs-up"></i></p>
             </div>
             <h5>${comment?.user?.username || "Anonymous"} 
-                <i class="fa-solid fa-circle-check"></i>
+                <i id="checkmark" class="fa-solid fa-circle-check"></i>
             </h5>
             <p id="revText">${comment?.body || "No review text available."}</p>
         </div>`;
@@ -180,11 +148,51 @@ async function displayBrands() {
     console.error("Error fetching brands:", error);
   }
 }
+displayBrands();
 
-// Run function after document is ready
-$(document).ready(() => {
-  displayBrands();
-});
+function counter() {
+  const stat1 = document.getElementById("stat1");
+  const stat2 = document.getElementById("stat2");
+  const stat3 = document.getElementById("stat3");
+
+  const target1 = 200;
+  const target2 = 2000;
+  const target3 = 30000;
+
+  let start1 = 0;
+  let start2 = 0;
+  let start3 = 0;
+
+  const steps = 100; // so that the numbers dont show up instantly
+
+  const increment1 = Math.ceil(target1 / steps); // remove decimals and increment values
+  const increment2 = Math.ceil(target2 / steps);
+  const increment3 = Math.ceil(target3 / steps);
+
+  function updateCounter() {
+    start1 = start1 + increment1;
+    start2 = start2 + increment2;
+    start3 = start3 + increment3;
+
+    stat1.textContent = start1;
+    stat2.textContent = start2;
+    stat3.textContent = start3;
+
+    if (start1 === target1 && start2 === target2 && start3 === target3) {
+      clearInterval(interval);
+    }
+  }
+
+  const interval = setInterval(updateCounter, 10); //execute on a 100 steps each step is 10ms
+}
+
+counter();
+
+async function displayCategory() {
+  const categroyText = document.querySelectorAll("#Categorytext");
+}
+
+displayCategory();
 
 shopButton = document.getElementById("shopButton");
 shopButton.addEventListener("click", function () {
@@ -197,9 +205,5 @@ productImgs1.addEventListener("click", function () {
 });
 productImgs2 = document.getElementById("products2");
 productImgs2.addEventListener("click", function () {
-  window.location.href = "http://127.0.0.1:5500/pages/products.html";
-});
-categories = document.getElementById("categoryBox");
-categories.addEventListener("click", function () {
   window.location.href = "http://127.0.0.1:5500/pages/products.html";
 });
